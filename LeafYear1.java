7c9202989ba1c75a28f19bfe56766f14a4263d12
Laxmiputra Pujari <162886831+pujari7@users.noreply.github.com>
2024-04-12 18:45:25 +0530

    Add files via upload

---
+++ LeafYear1.java
@@ -0,0 +1,17 @@
+package WheaterCheackPrograms;
+
+public class LeafYear1 {
+
+	public static void main(String[] args) {
+		int year=8;
+		if(year%4==0 && year%100!=0 || year%400==0)
+		{
+			System.out.println("Leaf Year");
+		}
+		else {
+			System.out.println("Not a Leaf Year");
+		}
+
+	}
+
+}
